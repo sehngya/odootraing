86732330323b104c71490f34ff6383139c52c976
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 15:01:09 +0000

    wsyday01

--- mypscloud_traing/models/subject.py
+++ mypscloud_traing/models/subject.py
@@ -10,6 +10,6 @@ class TrainingSubject(models.Model):
     preson = fields.Char(u'负责人',size=64,default=u'科目负责人',trantrate=True)
     #'person_id' = fields.Many2one('res.partner', u'负责人')
     desc = fields.Text(string='描述')
-    lesson_ids = fields.Many2one('mypscloud.traing.lesson', u'课程'),
+    lesson_ids = fields.Many2one('mypscloud.traing.lesson','subject_id', u'课程'),
     
     
