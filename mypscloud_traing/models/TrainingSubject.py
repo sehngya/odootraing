6f06c6f483badae63eb7790d4b3d1a55d139fac6
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 13:44:05 +0000

    wsyday01

--- mypscloud_traing/models/TrainingSubject.py
+++ mypscloud_traing/models/TrainingSubject.py
@@ -3,13 +3,13 @@
 from odoo import api, fields, models
 
 class TrainingSubject(Modes.model):
-    _name = 'mypscloud_traing.subject'
+    _name = 'mypscloud.traing.subject'
     _rec_name = 'name'
     
     name = fields.Char(u'科目名称'，size=64,default=u'科目名称',translate=True)
     preson = fields.Char(u'负责人'，size=64,default=u'科目负责人',trantrate=True)
     #'person_id' = fields.Many2one('res.partner', u'负责人')
     desc = fields.Text(string='描述')
-    lesson_ids = fields.Many2one('mypscloud_traing.lesson', u'课程'),
+    lesson_ids = fields.Many2one('mypscloud.traing.lesson', u'课程'),
     
     
