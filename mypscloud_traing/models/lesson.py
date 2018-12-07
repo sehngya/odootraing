2fb17aa014865ad9033c4d688d0391994cc9f5f3
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 15:40:23 +0000

    wsyday01

--- mypscloud_traing/models/lesson.py
+++ mypscloud_traing/models/lesson.py
@@ -11,7 +11,7 @@ class TrainingLesson(models.Model):
     start_date = fields.Date(string='开始时间')
     end_date = fields.Date(string='结束时间')
     continue_days = fields.Integer(string='持续天数', compute='_compute_days', store=True)
-    subject_id = fields.Many2one('mypscloud.traing.subject', string='科目')
+   # subject_id = fields.Many2one('mypscloud.traing.subject', string='科目')
     seat_qty = fields.Char(u'测试')
     #person_id = fields.Many2one('res.partner', related='subject_id.person_id', readonly=True)
     desc = fields.Text(string='描述')
