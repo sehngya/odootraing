b173bf136c3bf652713af5e1a03153d464b3d2d9
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 14:31:15 +0000

    wsyday01

--- mypscloud_traing/models/.ipynb_checkpoints/lesson-checkpoint.py
+++ mypscloud_traing/models/.ipynb_checkpoints/lesson-checkpoint.py
@@ -7,10 +7,11 @@ class TrainingLesson(models.Model):
     _rec_name = 'name'
     
     name = fields.Char(u'课程名称',size=64,default=u'课程名称',transtrate=True)
-    # teacher_id = Many2one('',u'教师')
+    teacher_id = fields(u'教师',size=64)
     start_date = fields.Date(string='开始时间')
     end_date = fields.Date(string='结束时间')
     continue_days = fields.Integer(string='持续天数', compute='_compute_days', store=True)
     subject_id = fields.Many2one('mypscloud.traing.subject', string='科目')
+    seat_qty = fields.Char(u'测试')
     #person_id = fields.Many2one('res.partner', related='subject_id.person_id', readonly=True)
     desc = fields.Text(string='描述')
