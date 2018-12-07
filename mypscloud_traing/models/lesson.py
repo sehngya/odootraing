ad35ef7c5bf729033123bcd753bf0641cd6a1f10
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 14:55:25 +0000

    wsyday01

--- mypscloud_traing/models/lesson.py
+++ mypscloud_traing/models/lesson.py
@@ -7,7 +7,7 @@ class TrainingLesson(models.Model):
     _rec_name = 'name'
     
     name = fields.Char(u'课程名称',size=64,default=u'课程名称',transtrate=True)
-    teacher_id = fields(u'教师',size=64)
+    teacher_id = fields.Char(u'教师',size=64)
     start_date = fields.Date(string='开始时间')
     end_date = fields.Date(string='结束时间')
     continue_days = fields.Integer(string='持续天数', compute='_compute_days', store=True)
