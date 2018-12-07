014bef1233f3b7d17c145946dd576b3a95d8ffc5
sehngya <32405002+sehngya@users.noreply.github.com>
2018-12-07 11:38:53 +0000

    wsyday01

--- mypscloud_traing/__manifest__.py
+++ mypscloud_traing/__manifest__.py
@@ -7,7 +7,7 @@
     'description': '培训测试',
 
     'author': '王圣亚',
-    'website': 'https://www.mypscloud.com/,
+    'website': 'https://www.mypscloud.com/',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
